fe289bcf92c9e764109de1d462df032f483a4648
guy7cc <alrdy.tree1sawa@gmail.com>
2022-06-09 04:48:29 +0900

    Replace examplemod with rpgwmod

---
+++ src/main/java/io/github/guy7cc/RpgwMod.java
@@ -0,0 +1,15 @@
+package io.github.guy7cc;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(RpgwMod.MOD_ID)
+public class RpgwMod
+{
+    public static final String MOD_ID = "rpgwmod";
+
+    public RpgwMod()
+    {
+        
+    }
+}
