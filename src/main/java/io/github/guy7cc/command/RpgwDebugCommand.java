592a9d6f27a03fd7ab236adaa65b421f578ede8d
guy7cc <alrdy.tree1sawa@gmail.com>
2022-12-23 13:09:44 +0900

    Create BlockStateJsonConverter

--- src/main/java/io/github/guy7cc/command/RpgwDebugCommand.java
+++ src/main/java/io/github/guy7cc/command/RpgwDebugCommand.java
@@ -1,23 +1,75 @@
 package io.github.guy7cc.command;
 
+import com.google.gson.JsonObject;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import io.github.guy7cc.RpgwMod;
+import io.github.guy7cc.block.entity.VendingMachineBlockEntity;
 import io.github.guy7cc.save.cap.PlayerMp;
 import io.github.guy7cc.syncdata.PlayerMpManager;
+import io.github.guy7cc.util.BlockStateJsonConverter;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class RpgwDebugCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(Commands.literal("rpgwdebug")
+                .requires(source -> source.hasPermission(2))
                 .then(Commands.literal("mp")
                         .then(Commands.argument("value", IntegerArgumentType.integer())
                                 .executes(ctx ->
                                         mp(ctx.getSource(), IntegerArgumentType.getInteger(ctx, "value")
                                         )
                                 )
+                        )
+                )
+                .then(Commands.literal("setBlockEntity")
+                        .then(Commands.argument("blockpos", BlockPosArgument.blockPos())
+                                .executes(ctx -> {
+                                    return setBlockEntity(ctx.getSource(), BlockPosArgument.getLoadedBlockPos(ctx, "blockpos"));
+                                })
+                        )
+                )
+                .then(Commands.literal("getBlockEntity")
+                        .then(Commands.argument("blockpos", BlockPosArgument.blockPos())
+                                .executes(ctx -> {
+                                    return getBlockEntity(ctx.getSource(), BlockPosArgument.getLoadedBlockPos(ctx, "blockpos"));
+                                })
+                        )
+                )
+                .then(Commands.literal("saveBlockState")
+                        .then(Commands.argument("blockpos", BlockPosArgument.blockPos())
+                                .then(Commands.argument("name", StringArgumentType.string())
+                                        .executes(ctx -> {
+                                            return saveBlockState(
+                                                    ctx.getSource(),
+                                                    BlockPosArgument.getLoadedBlockPos(ctx, "blockpos"),
+                                                    StringArgumentType.getString(ctx, "name")
+                                            );
+                                        })
+                                )
+                        )
+                )
+                .then(Commands.literal("setBlock")
+                        .then(Commands.argument("name", StringArgumentType.string())
+                                .executes(ctx -> {
+                                    return setBlock(ctx.getSource(), StringArgumentType.getString(ctx, "name"));
+                                })
                         )
                 )
         );
@@ -28,6 +80,57 @@ public class RpgwDebugCommand {
         PlayerMp playerMp = PlayerMpManager.getPlayerMp(player);
         if(playerMp != null){
             playerMp.setMp(mp);
+        }
+        return 1;
+    }
+
+    private static int setBlockEntity(CommandSourceStack source, BlockPos pos) throws CommandSyntaxException {
+        ServerPlayer player = source.getPlayerOrException();
+        ServerLevel level = player.getLevel();
+        level.setBlockEntity(new VendingMachineBlockEntity(pos, level.getBlockState(pos)));
+        return 1;
+    }
+
+    private static int getBlockEntity(CommandSourceStack source, BlockPos pos) throws CommandSyntaxException {
+        ServerPlayer player = source.getPlayerOrException();
+        ServerLevel level = player.getLevel();
+        BlockEntity be = level.getBlockEntity(pos);
+        if(be == null) player.displayClientMessage(new TextComponent("null"), false);
+        else player.displayClientMessage(new TextComponent(be.toString()), false);
+        return 1;
+    }
+
+    private static int saveBlockState(CommandSourceStack source, BlockPos pos, String name) throws CommandSyntaxException {
+        ServerLevel level = source.getLevel();
+        JsonObject obj = BlockStateJsonConverter.toJson(level, pos);
+        Path folderPath = Path.of(level.getStructureManager().generatedDir + "\\" + RpgwMod.MOD_ID + "\\blockstates");
+        Path filePath = Path.of(folderPath + "\\" + name + ".json");
+        try{
+            if(!Files.exists(folderPath)) Files.createDirectories(folderPath);
+            if(!Files.exists(filePath)) Files.createFile(filePath);
+            FileWriter fw = new FileWriter(filePath.toString(), false);
+            PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+            pw.print(obj.toString());
+            pw.close();
+        } catch(IOException e){
+            e.printStackTrace();
+            return 0;
+        }
+        source.sendSuccess(new TextComponent("Successfully saved the block state as a json file: " + filePath), true);
+        return 1;
+    }
+
+    private static int setBlock(CommandSourceStack source, String fileName){
+        ServerLevel level = source.getLevel();
+        Path filePath = Path.of(level.getStructureManager().generatedDir + "\\" + RpgwMod.MOD_ID + "\\blockstates\\" + fileName + ".json");
+        try{
+            if(!Files.exists(filePath)) return 0;
+            String text = Files.readString(filePath);
+            JsonObject obj = GsonHelper.parse(text);
+            BlockStateJsonConverter.setBlockFromJson(obj, level);
+        } catch(IOException e){
+            e.printStackTrace();
+            return 0;
         }
         return 1;
     }
---
+++ src/main/java/io/github/guy7cc/util/BlockStateJsonConverter.java
@@ -0,0 +1,107 @@
+package io.github.guy7cc.util;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.mojang.serialization.JsonOps;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateHolder;
+import net.minecraft.world.level.block.state.properties.Property;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+
+public class BlockStateJsonConverter {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static JsonObject toJson(BlockState state){
+        JsonObject root = new JsonObject();
+        root.addProperty("id", state.getBlock().getRegistryName().toString());
+        if(!state.getValues().isEmpty()){
+            JsonObject values = new JsonObject();
+            for(var entry : state.getValues().entrySet()){
+                values.addProperty(entry.getKey().getName(), entry.getValue().toString());
+            }
+            root.add("properties", values);
+        }
+        return root;
+    }
+
+    public static JsonObject toJson(ServerLevel level, BlockPos pos){
+        BlockState state = level.getBlockState(pos);
+        JsonObject root = toJson(state);
+        JsonArray jsonPos = new JsonArray();
+        jsonPos.add(pos.getX());
+        jsonPos.add(pos.getY());
+        jsonPos.add(pos.getZ());
+        root.add("pos", jsonPos);
+        if(state.hasBlockEntity()){
+            BlockEntity be = level.getBlockEntity(pos);
+            if(be != null){
+                CompoundTag tag = be.saveWithoutMetadata();
+                JsonElement tagObj = CompoundTag.CODEC.encodeStart(JsonOps.INSTANCE, tag).result().orElse(null);
+                if(tagObj != null){
+                    root.add("tag", tagObj);
+                }
+            }
+        }
+        return root;
+    }
+
+    public static void setBlockFromJson(JsonObject obj, ServerLevel level){
+        if(!obj.has("id")) LOGGER.warn("Cannot determine a block type because of not having the id in Json Object.");
+        Block block = Registry.BLOCK.get(new ResourceLocation(obj.get("id").getAsString()));
+        BlockPos pos = obj.has("pos") ? toBlockPos(obj.getAsJsonArray("pos")) : BlockPos.ZERO;
+        BlockState state = block.defaultBlockState();
+        if(obj.has("properties")){
+            for(var entry : obj.get("properties").getAsJsonObject().entrySet()){
+                String key = entry.getKey();
+                String value = entry.getValue().getAsString();
+                Property<?> property = block.getStateDefinition().getProperty(key);
+                state = setValueHelper(state, property, key, value);
+            }
+        }
+        level.setBlockAndUpdate(pos, state);
+        if(obj.has("tag") && block instanceof EntityBlock eb){
+            CompoundTag tag = CompoundTag.CODEC.parse(JsonOps.INSTANCE, obj.getAsJsonObject("tag")).result().orElse(null);
+            if(tag != null){
+                BlockEntity be = eb.newBlockEntity(pos, state);
+                level.setBlockEntity(be);
+                be.load(tag);
+                be.setChanged();
+            }
+        }
+    }
+
+    private static BlockPos toBlockPos(JsonArray array){
+        int[] arr = new int[3];
+        int i = 0;
+        for(JsonElement je : array){
+            arr[i] = je.getAsInt();
+            i++;
+        }
+        return new BlockPos(arr[0], arr[1], arr[2]);
+    }
+
+    private static <T extends Comparable<T>> BlockState setValueHelper(BlockState state, Property<T> property, String key, String value) {
+        Optional<T> optional = property.getValue(value);
+        if (optional.isPresent()) {
+            return state.setValue(property, optional.get());
+        } else {
+            LOGGER.warn("Unable to read property: {} with value: {} for blockstate: {}", key, value, state.getBlock().getRegistryName().toString());
+            return state;
+        }
+    }
+}
